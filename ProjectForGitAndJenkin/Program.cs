15e7615197fc26c74d43ae07e44b1ec14fcf0de9
Mayuri <mayurikandekar09>
2025-12-19 22:41:31 +0530

    Add project files.

---
+++ ProjectForGitAndJenkin/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectForGitAndJenkin
+{
+    internal class Program
+    {
+        public void Display() {
+
+            var a = 20;
+            var b = 10;
+            var c = a + b;
+            Console.WriteLine("Sum is: " + c);
+
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
